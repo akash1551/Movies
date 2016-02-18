ad1e14b7382edecf1e3a8276b6719fe2796b7afe
Nikhil Rane <nikhilrane1992@gamil.com>
2016-02-18 20:33:43 +0530

    change database path

--- Movies/movies/settings.py
+++ Movies/movies/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'movies.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../Project_Database/movies'),
     }
 }
 
