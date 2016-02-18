b0c93e1c926235cb2108dff9f5721f973819ec17
Girish <girishchaudhari76@gmail.com>
2016-02-18 16:37:47 +0530

    save data urls added.

--- Movies/movies/urls.py
+++ Movies/movies/urls.py
@@ -20,4 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^allmovie/',Movies.views.show_all_movies),
+    url(r'^savedata/',Movies.views.show_all_movies),
 ]
