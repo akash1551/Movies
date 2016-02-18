21813ff33bb31cdd1a024e9dcc15c7fee3f9a585
Akash <acwankhede@gmail.com>
2016-02-18 15:26:35 +0530

    created models for this project

---
+++ Movies/movies/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+admin.autodiscover()
+from models import Movie,Category,Relationship
+# Register your models here.
+
+admin.site.register(Movie)
+admin.site.register(Category)
+admin.site.register(Relationship)
--- Movies/movies/models.py
+++ Movies/movies/models.py
@@ -14,3 +14,45 @@ class Movie(models.Model):
 	releaseDate = models.DateField("%b %d %Y")
 
 class Category(models.Model):
+	language = models.IntegerField()
+	genre = models.IntegerField()
+
+class Language:
+	English = 0
+	Hindi = 1
+	Marathi = 2
+	Tamil = 3
+
+	LANGUAGECHOICES = ((English,"English"),(Hindi,"Hindi"),(Marathi,"Marathi"),(Tamil,"Tamil"))
+
+class Genre:
+	All = 0
+	Horror = 1
+	Indian = 2
+	Thriller = 3
+	Award_Winning = 4
+	Animation = 5
+	Romance = 6
+	Comedy = 7
+	Famous_Directors = 8
+	Fantasy = 9
+	Experimental = 10
+	Bed_Time_Stories = 11
+	Dark = 12
+	Drama = 13
+	Short_Doc = 14
+	Oscar_Winning = 15
+	Family = 16
+	Surreal = 17
+	Space = 18
+
+	GENRECHOICES = ((All,"All"),(Horror,"Horror"),(Indian,"Indian"),(Thriller,"Thriller"),
+		(Award_Winning,"Award Winning"),(Animation,"Animation"),(Romance,"Romance"),(Comedy,"Comedy"),
+		(Famous_Directors,"Famous Directors"),(Fantasy,"Fantasy"),(Experimental,"Experimental"),(Bed_Time_Stories,"Bed Time Stories"),
+		(Dark,"Dark"),(Drama,"Drama"),(Short_Doc,"Short Doc"),(Oscar_Winning,"Oscar Winning"),
+		(Family,"Family"),(Surreal,"Surreal"),(Space,"Space"))
+
+class Relationship(models.Model):
+	taxonomyId = models.ForeignKey('Category')
+	movieId = models.ForeignKey('Movie')
+	
