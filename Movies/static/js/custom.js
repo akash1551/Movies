9c29aec7d56971b24e40b9b8e87a386f296885b3
Nikhil Rane <nikhilrane1992@gamil.com>
2016-02-19 00:01:02 +0530

    send request with filter and get data from server

--- Movies/static/js/custom.js
+++ Movies/static/js/custom.js
@@ -16,6 +16,25 @@ angular.module("movieApp", ['ui.bootstrap'])
 .controller("movieCtrl", function($scope,$http){
 
     $scope.selectedFilterList = []
+    $scope.totalItems = 64;
+
+    $scope.currentPage = 1;
+
+    $scope.setPage = function (pageNo) {
+        $scope.currentPage = pageNo;
+    };
+
+    $scope.pageChanged = function() {
+        console.log('Page changed to: ' + $scope.currentPage);
+        console.log('Big Current Page: ' + $scope.bigCurrentPage);
+        console.log('Big Total Items: ' + $scope.bigTotalItems);
+        console.log('Max Size: ' + $scope.maxSize);
+    };
+
+    $scope.maxSize = 5;
+    $scope.bigTotalItems = 2;
+    $scope.bigCurrentPage = 1;
+
     var searchFilter = function(){
         $http({method: 'GET', url: '/filter/list/'})
             .then(function successCallback(response) {
@@ -26,15 +45,36 @@ angular.module("movieApp", ['ui.bootstrap'])
             });
     }
 
+    var getMovieList = function(){
+        $http.post('/movies_by_filter/',{pageNo: $scope.currentPage, totalEntries: $scope.bigTotalItems, selectedFilterList: $scope.selectedFilterList}).
+            success(function(data, status, headers, config) {
+                console.log(data);
+            }).
+            error(function(data, status, headers, config) {
+                console.log(data);
+            });
+    }
+
     var init = function(){
         searchFilter()
     }
     init();
 
     $scope.clickFilter = function(obj){
-        $scope.selectedFilterList.push(obj);
-        console.log($scope.selectedFilterList)
+        var checkbox = $('#'+obj.name+"-"+obj.id);
+        console.log(checkbox.is(":checked"))
+        if (checkbox.is(":checked") == false){
+            $scope.selectedFilterList.push(obj);
+            checkbox.prop('checked', true);
+            console.log($scope.selectedFilterList)
+            getMovieList();
+        }else{
+            $scope.selectedFilterList.pop(obj);
+            checkbox.prop('checked', false);
+            console.log($scope.selectedFilterList)
+            }
     }
+        
 
 
 });
